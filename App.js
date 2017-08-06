f4e344b47cb08b573511a1da2b3b2951a99ed812
Istabraq <angel.ofmoons@gmail.com>
2017-08-06 17:38:30 -0400

    first commit

---
+++ App.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+import Tasklist from './tasklist';
+import logo from './logo.png';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <div className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <h2><span className='red-text'>IST</span>oDo</h2>
+		  <h4>By Istabraq Almusally</h4>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default App;
